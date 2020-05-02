f59c684df1d98680123076364faf1145087247b9
DreamWebStar <james.kennedy870311@hotmail.com>
2020-05-02 05:17:54 +0300

    get and post request

--- src/main/java/com/itdev/rest/controller/BlogController.java
+++ src/main/java/com/itdev/rest/controller/BlogController.java
@@ -1,4 +1,4 @@
-package com.itdev.controller;
+package com.itdev.rest.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
